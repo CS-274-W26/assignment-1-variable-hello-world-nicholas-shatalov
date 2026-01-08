62f311ca29544929ea938f0199b64568a62895dd
Nicholas Shatalov <nicholas.shat@gmail.com>
2026-01-08 12:58:30 -0800

    Completed assignment

--- prog1.c
+++ prog1.c
@@ -1,11 +1,19 @@
-#include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+	srand(time(NULL));
+	int myrand = rand() % 11;
+	// printf("%d\n", myrand);
+
+	if(myrand >= 0 && myrand < 5) {
+		printf("Eat more beef, kick less cats\n");
+	}
+	else if(myrand > 4 && myrand < 10) {
+		printf("FRODO LIVES\n");
+	}
+	else if(myrand == 10) {
+		printf("Larn is the best roguelike\n");
+	}
 }
